aef330a6a8e1adb8c0997cd8056e3741b0ebd876
Richard Wei <xinranmsn@gmail.com>
2016-09-17 15:25:23 -0500

    Clear test

--- Tests/MTMarathonTests/MTMarathonTests.swift
+++ Tests/MTMarathonTests/MTMarathonTests.swift
@@ -2,16 +2,8 @@ import XCTest
 @testable import MTMarathon
 
 class MTMarathonTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(MTMarathon().text, "Hello, World!")
-    }
-
-
     static var allTests : [(String, (MTMarathonTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
         ]
     }
 }
